d62f08a4ae627a6ef1c583c368ca41bf47a5bd1d
Sohail <sohailkarmani16@gmail.com>
2025-10-04 23:03:44 +0500

    update rentalsystem class

--- CarRentalSystem.java
+++ CarRentalSystem.java
@@ -1,3 +1,46 @@
+import java.util.List;
+
 public class CarRentalSystem {
-    
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
+
+    public CarRentalSystem(){
+        cars=new arraylist<>;
+        customers=new arraylist<>;
+        rentals=new arraylist<>;
+    }
+    public void addCar(){
+        cars.add(car);
+    }
+    public void addCustomer(){
+        customers.add(customer);
+    }
+    public void rentCar(Car car,Customer customer,int days){
+        if(car.isAvailable()){
+            car.rent();
+            rentals.add(new Rental(car, customer, days));
+
+        }
+        else{
+            System.out.println("Car is not avialable for rent");
+        }
+    }
+    public void returnCar(Car car){
+        car.ReturnCar();
+        Rental rentalRemove=null;
+        for(Rental rental:rentals){
+            if(rental.getCar()==car){
+                rentalRemove=rental;
+                break;
+            }
+        }
+        if(rentalRemove!=null){
+            rentals.remove(rentalRemove);
+
+        }
+        else{
+            System.out.println("Car was not rented.");
+        }
+    }
 }
